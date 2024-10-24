350e4e6c3bbc8279fe61fdb88b53459d469e3736
Dawoork <lean.araque@gmail.com>
2024-10-23 23:02:41 -0300

    test

--- function_app.py
+++ function_app.py
@@ -1,5 +1,22 @@
 import azure.functions as func
 import logging
+from google.analytics.data_v1beta import BetaAnalyticsDataClient
+from google.oauth2 import service_account
+import os
+import json
+from azure.storage.blob import BlobServiceClient
+
+def get_ga4_client():
+    credentials_info = json.loads(os.environ['GOOGLE_APPLICATION_CREDENTIALS_JSON'])
+    credentials = service_account.Credentials.from_service_account_info(credentials_info)
+    client = BetaAnalyticsDataClient(credentials=credentials)
+    return client
+
+def upload_to_blob_storage(data):
+    blob_service_client = BlobServiceClient.from_connection_string(os.environ['BLOB_CONNECTION_STRING'])
+    container_name = os.environ['BLOB_CONTAINER_NAME']
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob='ga4_data.json')
+    blob_client.upload_blob(data, overwrite=True)
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
