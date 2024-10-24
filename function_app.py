f40bfcf4a9b2e3ff79caf15134e691854260a066
Dawoork <lean.araque@gmail.com>
2024-10-23 22:54:51 -0300

    test

--- function_app.py
+++ function_app.py
@@ -20,6 +20,6 @@ def AdeemyFuntionAds(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "Keeeev function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "Keeeev. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
